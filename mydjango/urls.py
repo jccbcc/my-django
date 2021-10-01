c852f060d7163f7c4729cfa3c82bc607f739845d
Jason Coleman <jccbcc99@gmail.com>
2021-10-01 13:26:14 -0500

    Add Initial View

--- mydjango/urls.py
+++ mydjango/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from django.contrib import admin
+
+from boards import views
 
 urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    url(r'^admin/', admin.site.urls),
     path('admin/', admin.site.urls),
 ]
